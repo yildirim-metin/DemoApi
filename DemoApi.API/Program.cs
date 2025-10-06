b434cb3a1d8f6fa467c8e8c7056f32e3ad70eef1
metin yildirim <metin_yildirim@outlook.be>
2025-10-06 11:54:38 +0200

    configure launch settings in vscode using swagger

--- DemoApi.API/Program.cs
+++ DemoApi.API/Program.cs
@@ -1,3 +1,6 @@
+using DemoApi.BLL.Services;
+using DemoApi.BLL.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,12 +9,19 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddScoped<IBookService, BookService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "OpenApi V1");
+    });
 }
 
 app.UseHttpsRedirection();
